58df5cbf0a694852e541108c1d5aade3795365e6
jojojangles <jcotosax@gmail.com>
2014-09-07 01:34:48 -0400

    make paddle move

---
+++ Assets/scripts/paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class paddle : MonoBehaviour {
+	public float paddleSpeed;
+	// Use this for initialization
+	void Start () {
+		paddleSpeed = .5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxisRaw ("Horizontal") > 0 && gameObject.transform.position.x < 25){
+			gameObject.transform.Translate(0, 0, paddleSpeed);
+		}
+		if(Input.GetAxisRaw ("Horizontal") < 0 && gameObject.transform.position.x > -25){
+			gameObject.transform.Translate(0, 0, -paddleSpeed);
+		}
+	}
+}

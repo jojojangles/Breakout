1c014122a4a874557fbfff1d26f9ec0046fa4889
jojojangles <jcotosax@gmail.com>
2014-09-07 21:47:00 -0400

    all components + first person

---
+++ Assets/scripts/fpcam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fpcam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 padloc = GameObject.FindGameObjectWithTag("paddle").transform.position;
+		Vector3 camloc = transform.position;
+		transform.position = new Vector3(padloc.x,-33,-6);
+	}
+}

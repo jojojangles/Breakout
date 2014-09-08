dbe7ac98b6f6367dc5d6408a4998f2d643439458
jojojangles <jcotosax@gmail.com>
2014-09-08 18:42:24 -0400

    Cam Toggle Son

--- Assets/scripts/core.cs
+++ Assets/scripts/core.cs
@@ -5,15 +5,28 @@ public class core : MonoBehaviour {
 	public GameObject blockPrefab;
 	public static int score = 0;
 	public static int lives = 3;
+	public Camera maincam;
+	public Camera firstperson;
 
 	// Use this for initialization
 	void Start () {
+		maincam.enabled = true;
+		firstperson.enabled = false;
 		blockMaker();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.G)){
+			if(maincam.enabled){
+				firstperson.enabled = true;
+				maincam.enabled = false;
+			}
+			else{
+				firstperson.enabled = false;
+				maincam.enabled = true;
+			}
+		}
 	}
 
 	void OnGUI(){
